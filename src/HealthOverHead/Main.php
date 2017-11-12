2b202d32a1d522ffe28821a4ed41993c1f3738c3
CookieCode <etienne.crosnier.la@gmail.com>
2017-11-12 12:31:59 +0100

    Update for the 3.0.0-ALPHA9

--- src/HealthOverHead/Main.php
+++ src/HealthOverHead/Main.php
@@ -1,20 +1,19 @@
 <?php
 
 namespace HealthOverHead;
-use pocketmine\command\Command;
-use pocketmine\command\CommandSender;
-use pocketmine\command\CommandExecutor;
-use pocketmine\Player;
+
+//Commands
+use pocketmine\command\{Command, CommandSender, CommandExecutor};
 use pocketmine\plugin\PluginBase;
-use pocketmine\Server;
-use pocketmine\utils\TextFormat;
-use pocketmine\utils\Config;
+use pocketmine\utils\{TextFormat, Config};
 use pocketmine\permission\ServerOperator;
-use pocketmine\event\player\PlayerCommandPreprocessEvent;
+//Events
 use pocketmine\event\Listener;
-use pocketmine\event\player\PlayerJoinEvent;
-use pocketmine\scheduler\PluginTask;
+use pocketmine\event\player\{PlayerJoinEvent, PlayerCommandPreprocessEvent};
 use pocketmine\event\entity\EntityDamageEvent;
+//Mains
+use pocketmine\scheduler\PluginTask;
+use pocketmine\{Player, Server};
 
  /**
  *  _____   ______   ______   _  _   _   ______
@@ -28,29 +27,27 @@ use pocketmine\event\entity\EntityDamageEvent;
 
 class Main extends PluginBase implements Listener{
   
-    public function onEnable(){
-        $this->getLogger()->info(TextFormat::BLUE . "HealthOverHead coded by CookieCode enabled.");
-		    $this->getServer()->getPluginManager()->registerEvents($this,$this);
+	public function onEnable(){
+       		$this->getLogger()->info(TextFormat::BLUE . "HealthOverHead by CookieCode enabled.");
+		$this->getServer()->getPluginManager()->registerEvents($this,$this);
 	    	$this->getServer()->getScheduler()->scheduleRepeatingTask(new Funcion($this),10);
-    }
+	}
     
-    public function onDisable(){
+	public function onDisable(){
         $this->getLogger()->info(TextFormat::BLUE . "HealthOverHead disabled.");
-    }
+	}
 	
-    public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool{
-        switch($command->getName()){
+	public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args): bool{
+	switch($cmd->getName()){
  
-            case "healthoverhead":
+		case "healthoverhead":
 
-$sender->sendMessage(TextFormat::YELLOW."------------");
-$sender->sendMessage(TextFormat::GREEN."HealthOverHead created by CookieCode. Twitter : @ImCookieGame");
-$sender->sendMessage(TextFormat::RED."Youtube : ImCookieGame");
-$sender->sendMessage(TextFormat::YELLOW."------------");
-
-
-return true;
-
-}
-}
+		$sender->sendMessage(TextFormat::YELLOW."------------");
+		$sender->sendMessage(TextFormat::GREEN."HealthOverHead created by CookieCode. Twitter : @ImCookieGame");
+		$sender->sendMessage(TextFormat::RED."Youtube : ImCookieGame");
+		$sender->sendMessage(TextFormat::YELLOW."------------");
+		
+		return true;
+		}
+	}
 }
